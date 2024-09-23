8eaebc90564e83bad3df54c7b7c552050dbf5046
Eduardo <eandrade.0010@gmail.com>
2024-09-22 18:19:13 -0700

    Removed debugging print statement

--- src/QuoteEngine/Ingestor.py
+++ src/QuoteEngine/Ingestor.py
@@ -20,5 +20,4 @@ class Ingestor(IngestorInterface):
         """Return the appropriate parser from a collection of strategy objects for parsing appropriate file."""
         for ingestor in cls.ingestors:
             if ingestor.can_ingest(path):
-                print(ingestor.parse(path))
                 return ingestor.parse(path)
